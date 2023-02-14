ccac323097ea9e7ebf6149aa1131676f0d1054de
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-02-14 06:11:25 -0500

    adding auth

--- middleware/userLogin.js
+++ middleware/userLogin.js
@@ -11,10 +11,39 @@ const userLogin = async (req, res, next) => {
   const user = await getUserByUsername(username);
   if (!user.message) {
     const validPass = await bcrypt.compare(req.body.password, user.password);
-    console.log(validPass);
-    next();
+    if (validPass) {
+      //generate access token
+      req.token = jwt.sign(
+        { user: user.username, email: user.email },
+        process.env.SECRET_TOKEN,
+        {
+          expiresIn: "1h",
+        }
+      );
+      //generate refresh token
+      req.refreshToken = jwt.sign(
+        { user: user.username, email: user.email },
+        process.env.SECRET_RTOKEN,
+        {
+          expiresIn: "30d",
+        }
+      );
+      req.content = {
+        user: user.username,
+        email: user.email,
+      };
+      //add to list
+      refreshList[req.refreshToken] = {
+        status: "loggedin",
+        token: req.token,
+        refreshToken: req.refreshToken,
+      };
+      next();
+    } else {
+      res.status(400).json({ error: "Invalid password" });
+    }
   } else {
-    res.json({ error: `No account linked to ${username}` });
+    res.status(401).json({ error: `No account linked to ${username}` });
   }
 };
 
